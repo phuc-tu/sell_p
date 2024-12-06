a01f0d79bbfb836a14e4db274d9b057e1b69e8c3
akai <nguyenphuctu.0904@gmail.com>
2024-12-07 05:18:34 +0700

    ét

--- js/admin/admin.js
+++ js/admin/admin.js
@@ -1,20 +1,20 @@
-var productPage = document.querySelector(".admin__product");
-var orderPage = document.querySelector(".admin__order");
-var statisticsPage = document.querySelector(".admin__statis");
-var userPage = document.querySelector(".admin__user");
-var contentBtn = document.querySelectorAll(".side-bar__item");
-var content = document.querySelector(".admin__content");
-var detailBill = document.querySelector(".admin__detail");
-localStorage.setItem("userAccountIndex", "");
+var productPage = document.querySelector('.admin__product');
+var orderPage = document.querySelector('.admin__order');
+var statisticsPage = document.querySelector('.admin__statis');
+var userPage = document.querySelector('.admin__user');
+var contentBtn = document.querySelectorAll('.side-bar__item');
+var content = document.querySelector('.admin__content');
+var detailBill = document.querySelector('.admin__detail');
+localStorage.setItem('userAccountIndex', '');
 function showCurrentContent(name) {
     var index;
     for (var i = 0; i < contentBtn.length; i++) {
-        contentBtn[i].classList.remove("active");
-        if (contentBtn[i].getAttribute("value") == name) {
+        contentBtn[i].classList.remove('active');
+        if (contentBtn[i].getAttribute('value') == name) {
             index = i;
         }
     }
-    contentBtn[index].classList.add("active");
+    contentBtn[index].classList.add('active');
 }
 function toggleDisplayThongke(showClass, hideClass) {
     // Lấy các phần tử cần hiển thị và ẩn
@@ -22,39 +22,39 @@ function toggleDisplayThongke(showClass, hideClass) {
     const hideElement = document.querySelector(`.${hideClass}`);
 
     // Kiểm tra và thay đổi trạng thái hiển thị
-    if (showElement) showElement.style.display = "block";
-    if (hideElement) hideElement.style.display = "none";
+    if (showElement) showElement.style.display = 'block';
+    if (hideElement) hideElement.style.display = 'none';
 }
 function backHomePage() {
-    window.location.href = "index.html";
+    window.location.href = 'index.html';
 }
 
 // Logout
 function LogOut() {
-    localStorage.setItem("isLogIn", 0);
-    localStorage.setItem("userAccountIndex", "");
-    window.location.href = "index.html";
+    localStorage.setItem('isLogIn', 0);
+    localStorage.setItem('userAccountIndex', '');
+    window.location.href = 'index.html';
 }
 
 // Tránh đóng modal khi thao tác trên modal-body
-var modalBodies = document.querySelectorAll(".modal-body");
+var modalBodies = document.querySelectorAll('.modal-body');
 
-modalBodies.forEach(function (modalBody) {
-    modalBody.addEventListener("click", function (event) {
+modalBodies.forEach(function(modalBody) {
+    modalBody.addEventListener('click', function(event) {
         event.stopPropagation();
-    });
-});
+    })
+})
 
 // Hủy delete
 function cacelDelete() {
-    productControlModal.style.display = "none";
-    userControlModal.style.display = "none";
+    productControlModal.style.display = 'none';
+    userControlModal.style.display = 'none';
 }
 
 // Side bar
-var switchBtn = document.querySelector(".side-bar-switch");
-var sideBar = document.querySelector(".side-bar");
+var switchBtn = document.querySelector('.side-bar-switch');
+var sideBar = document.querySelector('.side-bar');
 
-switchBtn.addEventListener("click", function () {
-    sideBar.classList.toggle("side-bar--close");
+switchBtn.addEventListener('click', function() {
+    sideBar.classList.toggle('side-bar--close');
 });
